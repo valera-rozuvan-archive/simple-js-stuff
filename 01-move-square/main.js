2ce6db95147f90b1a6dbb98257d5993663ccb759
Valera Rozuvan <valera.rozuvan@gmail.com>
2018-06-18 06:25:48 +0300

    Update demo "01 Move Square".

--- 01-move-square/main.js
+++ 01-move-square/main.js
@@ -1,9 +1,8 @@
 $(window).ready(function () {
   var canvas,
     ctx,
-    playerimage,
-    x,
-    y,
+    playerX,
+    playerY,
     player_steps = 10, // steps for the player to move by
     width = 10, // width of the player
     height = 10; // height of the player
@@ -11,12 +10,12 @@ $(window).ready(function () {
   init();
 
   function init() {
-    canvas = $("#my_canvas");
+    canvas = $('#my_canvas');
     ctx = canvas[0].getContext('2d');
-    x = canvas.width() / 2;
-    y = canvas.height() / 2;
+    playerX = canvas.width() / 2;
+    playerY = canvas.height() / 2;
 
-    window.addEventListener("keydown", update, false);
+    window.addEventListener('keydown', update, false);
     render();
   }
 
@@ -24,20 +23,20 @@ $(window).ready(function () {
     clear();
 
     if (event.key == 'ArrowLeft') {
-      x -= player_steps; //going left
+      playerX -= player_steps; // going left
     }
     if (event.key == 'ArrowRight') {
-      x += player_steps; //going right
+      playerX += player_steps; // going right
     }
 
     render();
   }
 
   function clear() {
-    ctx.clearRect(x, y, width, height);
+    ctx.clearRect(playerX, playerY, width, height);
   }
 
   function render() {
-    ctx.fillRect(x, y, width, height);
+    ctx.fillRect(playerX, playerY, width, height);
   }
 });
